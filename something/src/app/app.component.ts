a074a080ce086114bedb0a1c600ae8ce1388c2fb
g0lem <sasteve999@gmail.com>
2017-09-28 17:31:37 +0300

    working, kind of

---
+++ something/src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component } from '@angular/core';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app works!';
+  items = [];
+  new_item = {  };
+
+  constructor(){
+
+  	this.items = JSON.parse(window.localStorage.getItem('items'));
+
+  }
+
+
+  pushItem(){
+
+  	this.items.push(this.new_item);
+  	this.new_item = {};
+  	window.localStorage.setItem('items', JSON.stringify(this.items));
+  	console.log(window.localStorage.getItem('items'));
+  	
+  }
+}
