9b5b17a748e036c9358aa18b6b4e3b8fbd8582fa
g0lem <sasteve999@gmail.com>
2017-09-29 11:48:28 +0300

    dada

--- something/src/app/post/post.component.ts
+++ something/src/app/post/post.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
 
 @Component({
   selector: 'app-post',
@@ -7,7 +9,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PostComponent implements OnInit {
 
-  constructor() { console.log('die'); }
+  item;
+
+  constructor(private activatedRoute: ActivatedRoute) { 
+
+  	this.activatedRoute.params.subscribe(function(params: Params){
+        let id = params['id'];
+        console.log(id);
+        this.item = JSON.parse(window.localStorage.getItem('items'))[id];
+        console.log(this.item);
+    });
+
+
+  }
 
   ngOnInit() {
   }
